55dfcb9c9d70d1f8ea9d5841cf1d97c99b4365c8
Ilya Sitnikov <31476786+sitnikovik@users.noreply.github.com>
2025-12-28 08:52:38 +0300

    feat(style): add ANSI text style consts (#8)

---
+++ ansi/color/text/style.go
@@ -0,0 +1,12 @@
+package text
+
+const (
+	// Bold - the ANSI escape code for bold text.
+	Bold = "\033[1m"
+	// Dim - the ANSI escape code for dim text.
+	Dimm = "\033[2m"
+	// Italic - the ANSI escape code for italic text.
+	Italic = "\033[3m"
+	// Underline - the ANSI escape code for underline text.
+	Underline = "\033[4m"
+)
