58f69f12962c32116bfa0cf142d6e732e0a67f8d
Ilya Sitnikov <31476786+sitnikovik@users.noreply.github.com>
2025-12-28 08:43:16 +0300

    feat(colors): add base ANSI colors consts (#7)

---
+++ ansi/color/text/colors.go
@@ -0,0 +1,16 @@
+package text
+
+const (
+	// Red - the ANSI escape code for red text.
+	Red = "\033[31m"
+	// Green - the ANSI escape code for green text.
+	Green = "\033[32m"
+	// Yellow - the ANSI escape code for yellow text.
+	Yellow = "\033[33m"
+	// Blue - the ANSI escape code for blue text.
+	Blue = "\033[34m"
+	// Purple - the ANSI escape code for purple text.
+	Purple = "\033[35m"
+	// Cyan - the ANSI escape code for cyan text.
+	Cyan = "\033[36m"
+)
