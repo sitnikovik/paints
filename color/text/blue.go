a18a9094e1f947d21e775e0abcd6566f93bae0e0
Ilya Sitnikov <sitnikovik@gmail.com>
2025-12-28 09:19:52 +0300

    feat: add func to color text in blue

---
+++ color/text/blue.go
@@ -0,0 +1,16 @@
+package text
+
+import (
+	ansi "github.com/sitnikovik/paints/ansi/color"
+	ansiText "github.com/sitnikovik/paints/ansi/color/text"
+)
+
+// Blue returns a new string with blue text.
+//
+// Wraps the provided string with ANSI codes on the left and right.
+func Blue(s string) string {
+	if s == "" {
+		return ""
+	}
+	return ansiText.Blue + s + ansi.Reset
+}
