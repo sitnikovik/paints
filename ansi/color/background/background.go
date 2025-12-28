8fca58248c25ed58c6db41ea3b6c5e693dd88beb
Ilya Sitnikov <31476786+sitnikovik@users.noreply.github.com>
2025-12-28 08:59:17 +0300

    feat(background): add ANSI consts (#10)

---
+++ ansi/color/background/background.go
@@ -0,0 +1,16 @@
+package background
+
+const (
+	// Red - the ANSI escape code for red background.
+	Red = "\033[41m"
+	// Green - the ANSI escape code for green background.
+	Green = "\033[42m"
+	// Yellow - the ANSI escape code for yellow background.
+	Yellow = "\033[43m"
+	// Blue - the ANSI escape code for blue background.
+	Blue = "\033[44m"
+	// Purple - the ANSI escape code for purple background.
+	Purple = "\033[45m"
+	// Cyan - the ANSI escape code for cyan background.
+	Cyan = "\033[46m"
+)
