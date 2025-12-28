2716337d356fb18381de29d9897a6c3ae7e82a00
Ilya Sitnikov <sitnikovik@gmail.com>
2025-12-28 09:18:40 +0300

    feat: add func to color text in yellow

---
+++ color/text/yellow.go
@@ -0,0 +1,16 @@
+package text
+
+import (
+	ansi "github.com/sitnikovik/paints/ansi/color"
+	ansiText "github.com/sitnikovik/paints/ansi/color/text"
+)
+
+// Yellow returns a new string with yellow text.
+//
+// Wraps the provided string with ANSI codes on the left and right.
+func Yellow(s string) string {
+	if s == "" {
+		return ""
+	}
+	return ansiText.Yellow + s + ansi.Reset
+}
