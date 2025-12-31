5e5eed542a9aea49c5d6ab7b153c0f4ee46fb3d3
Ilya Sitnikov <sitnikovik@gmail.com>
2025-12-31 10:09:08 +0300

    feat: add func to color bacground in red

---
+++ color/background/red.go
@@ -0,0 +1,16 @@
+package background
+
+import (
+	ansi "github.com/sitnikovik/paints/ansi/color"
+	ansiBackground "github.com/sitnikovik/paints/ansi/color/background"
+)
+
+// Red returns a new string with red background.
+//
+// Wraps the provided string with ANSI codes on the left and right.
+func Red(s string) string {
+	if s == "" {
+		return ""
+	}
+	return ansiBackground.Red + s + ansi.Reset
+}
