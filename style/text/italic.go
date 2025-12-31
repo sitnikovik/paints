030b3e3b5d5bcc6f3f4a5eff18d4a8e499306ef3
Ilya Sitnikov <sitnikovik@gmail.com>
2025-12-31 10:44:12 +0300

    feat: add func to make text italic

---
+++ style/text/italic.go
@@ -0,0 +1,16 @@
+package text
+
+import (
+	ansi "github.com/sitnikovik/paints/internal/ansi/color"
+	ansiTextStyle "github.com/sitnikovik/paints/internal/ansi/style/text"
+)
+
+// Italic returns a new string with italic text style.
+//
+// Wraps the provided string with ANSI codes on the left and right.
+func Italic(s string) string {
+	if s == "" {
+		return ""
+	}
+	return ansiTextStyle.Italic + s + ansi.Reset
+}
